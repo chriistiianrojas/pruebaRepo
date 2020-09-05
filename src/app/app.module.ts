3eebc20b5ebaa284724aa7124e6927cf154d1302
chriistiianrojas <christianrojas2338@gmail.com>
2020-09-05 11:19:56 -0500

    [COMP] Crear componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Feature2Component } from './feature2/feature2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Feature2Component
   ],
   imports: [
     BrowserModule,
